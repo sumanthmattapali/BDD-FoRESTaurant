fa3ad8fe760d16619aa1fd5ff6507f699adc5d76
Aeonitis <aeonitis@gmail.com>
2017-06-05 21:55:56 +0100

    Unit tests, empty as of now

---
+++ src/test/java/com/bdd/springcucumber/test/ForestaurantApplicationTests.java
@@ -0,0 +1,19 @@
+package com.bdd.springcucumber.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Unit Testing
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ForestaurantApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
